90a3f01f0a62b65244dc9fc9645866a609974c9f
Mohsen Mirhosseini <mohsen.mirhosseini@gmail.com>
2021-01-23 08:47:10 +0330

    اضافه شدن حالت تغییر مشخصات پرونده

--- resources/js/Pages/Dashboard/Profiles/ViewProfile.vue
+++ resources/js/Pages/Dashboard/Profiles/ViewProfile.vue
@@ -71,9 +71,9 @@
                                             ثبت پرونده
                                             جدید
                                         </jet-button>
-                                        <jet-button :class="{'bg-blue-600':profileTypeForm.type==='EDIT'}"
+                                        <jet-button :class="{'bg-red-600':profileTypeForm.type==='EDIT'}"
                                                     @click.native="profileTypeForm.type='EDIT'"
-                                                    class="bg-blue-300 hover:bg-blue-400 active:bg-blue-800"
+                                                    class="bg-red-300 hover:bg-red-400 active:bg-red-800"
                                                     :disabled="$page.user.level!=='OFFICE' && $page.user.level!=='ADMIN' && $page.user.level!=='SUPERUSER' && profile.status!=0">
                                             تغییر مشخصات پرونده
                                         </jet-button>
