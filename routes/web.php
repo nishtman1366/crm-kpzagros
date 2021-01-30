ed4d60d76650533ac4625f1743858bbba8ef27fe
Mohsen Mirhosseini <mohsen.mirhosseini@gmail.com>
2021-01-30 12:52:47 +0330

    رفع باگ جابجایی سریال

--- routes/web.php
+++ routes/web.php
@@ -220,6 +220,6 @@ Route::get('listSerials', function () {
         ->where('new_device_id', '!=', null)
         ->get()->each(function ($profile) {
             $newDevice = Device::where('id', $profile->new_device_id)->get()->first();
-            echo $profile->customer->fullName . '-' . $profile->customer->national_code . '-' . (!is_null($profile->device) ? $profile->device->serial : '') . '-' . (!is_null($newDevice) ? $newDevice->serial : '');
+            echo '<p>'.$profile->customer->fullName . '-' . $profile->customer->national_code . '-' . (!is_null($profile->device) ? $profile->device->serial : '') . '-' . (!is_null($newDevice) ? $newDevice->serial : '').'</p>';
         });
 });
