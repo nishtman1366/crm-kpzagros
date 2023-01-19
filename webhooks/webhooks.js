a165e18f6de1478611dd306b9807dd7419195cef
Mohsen Mirhosseini <mohsen.mirhosseini@gmail.com>
2023-01-19 17:39:19 +0330

    وب‌هوک گیت‌هاب

--- webhooks/webhooks.js
+++ webhooks/webhooks.js
@@ -1,11 +1,17 @@
 const port = 3032
 const secret = "Nil00f@r1869";
-const repo = "/var/www/crm-kpzagros";
+const repo = "/home/nishtman/Projects/kpzagros";
 
 let http = require('http');
 let crypto = require('crypto');
 
-const exec = require('child_process').exec;
+// const exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
+const {execSync} = require('child_process');
+const {promisify} = require('util');
+// const exec = promisify(execWithCallback);
+
+const fs = require("fs");
 
 
 const commands = [
@@ -19,12 +25,34 @@ const commands = [
 ];
 
 http.createServer(function (req, res) {
-    req.on('data', function (chunk) {
-        let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
+    const writeStream = fs.createWriteStream("./storage/logs/webhooks/logs.txt", {flags: 'a'});
 
-        if (req.headers['x-hub-signature'] == sig) {
-            exec(commands.join(' && '));
-        }
+    req.on('data', function (chunk) {
+        let now = new Date();
+        let date = `${now.getFullYear()}-${now.getMonth()}-${now.getDay()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+
+        commands.map(function (command) {
+            try {
+                writeStream.write(`********** ${date} ********** \n`)
+                writeStream.write(`${date} ${command} \n`)
+                const error = execSync(command).toString();
+                writeStream.write(`${date} ${error} \n \n`)
+            }catch (error){
+                console.log('############################################')
+                console.log(error)
+                console.log('############################################')
+            }
+        })
+
+        // let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
+        //
+        // if (req.headers['x-hub-signature'] == sig) {
+        //     writeStream.write(`Running Commands...`);
+        //     exec(commands.join(' && '));
+        // }
+        writeStream.write(`--------------------------------------------------------------------------------------------------------------------------------------------------------------------- \n\n`);
+        writeStream.end();
+
     });
 
     res.end();
